8a48ae09e38a034ae9bc30615362a33a04af72ee
Prancing Pony Pub Local <local@prancingponypub.invalid>
2026-07-04 23:08:20 +0200

    Fixes for 404.html

--- .github/pages-root-files.sh
+++ .github/pages-root-files.sh
@@ -19,3 +19,18 @@ for root_file in overrides/robots.txt overrides/sitemap.xml "$pages_root"/mesbg2
     cp "$root_file" "$pages_root"/
   fi
 done
+
+if [ -f "$pages_root"/404.html ]; then
+  sed -i \
+    -e 's#href="/assets/#href="/mesbg2024/assets/#g' \
+    -e 's#src="/assets/#src="/mesbg2024/assets/#g' \
+    -e 's#href="/stylesheets/#href="/mesbg2024/stylesheets/#g' \
+    -e 's#src="/stylesheets/#src="/mesbg2024/stylesheets/#g' \
+    -e 's#href="/\."#href="/mesbg2024/"#g' \
+    -e 's#new URL("/",location)#new URL("/mesbg2024/",location)#g' \
+    "$pages_root"/404.html
+
+  for page_root in quick_start rules_manual matched_play_guide battle_companies forces_of_good forces_of_evil legendary_legions campaigns scenarios; do
+    sed -i "s#href=\"/$page_root/#href=\"/mesbg2024/$page_root/#g" "$pages_root"/404.html
+  done
+fi
